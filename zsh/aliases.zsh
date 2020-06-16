aab78d807310aba3e60cb5a437cfa0f8db7153f9
zlj-zz <zlj19971222@outlook.com>
2020-06-17 06:23:50 +0800

    modify ranger alias added mapping for lazygit in ranger

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,14 +2,13 @@
 alias c=clear -x
 alias cs=cowsay
 alias sudo='sudo -E'
-alias dcursor='echo -ne "\e[5 q"'
 #alias s="neofetch --cpu_temp C --memory_display barinfo --shell_path on "
 #alias s="neofetch --ascii_distro bilibili "
 alias s='neofetch --cpu_temp C --memory_display barinfo --shell_path on --ascii "$(fortune | cowsay -W 35 | lolcat)"'
 alias v="nvim"
 alias h='htop'
-alias r='ranger'
-alias sr='sudo -E ranger'
+alias ra='ranger'
+alias sra='sudo -E ranger'
 alias t='tmux'
 alias lg='lazygit'
 # git
