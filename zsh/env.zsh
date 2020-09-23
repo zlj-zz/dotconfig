cb6d53148f7980e640b53df80494d71561a1584d
Zachary Zhang <zlj19971222@outlook.com>
2020-09-23 13:46:02 +0800

    modify(ranger,zsh):

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,8 +2,6 @@ export XDG_CONFIG_HOME=$HOME/.config
 export LOCALBIN=$XDG_CONFIG_HOME/bin
 export PATH=$PATH:$LOCALBIN
 #export LOCALPROG=$HOME/prog
-#export GOPATH=$HOME/go
-export PATH=$PATH:$HOME/.gem/ruby/2.7.0
 export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:/home/zachary/.zachary/bin
@@ -12,19 +10,23 @@ export PATH=$PATH:$HOME/.zachary/bin
 export PATH=$PATH:$HOME/.zachary/sbin
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/snap/bin
-#export TERM=xterm-256color
-#export PATH=$PATH:$LOCALPROG/flutter/bin
-#export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
+export TERM=xterm-256color
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 export TERM=xterm-256color
 export TERM_ITALICS=true
 export RANGER_LOAD_DEFAULT_RC="false"
-#export TERM=screen-256color
+export TERM=screen-256color
 export TERMINFO=/usr/share/terminfo
 export EDITOR=/usr/bin/nvim
 export VISUAL=/usr/bin/nvim
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+#export PATH=$PATH:$LOCALPROG/flutter/bin
+#export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
+#export GOPATH=$HOME/go
+#export PATH=$PATH:$HOME/.gem/ruby/2.7.0
+
+source $HOME/.profile
 
 ## >>> conda initialize >>>
 ## !! Contents within this block are managed by 'conda init' !!
