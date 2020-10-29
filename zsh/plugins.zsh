348dec32cfa4af55c05016849aa07ca133f04c4e
Zachary Zhang <zlj19971222@outlook.com>
2020-10-30 00:05:32 +0800

    update plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,7 @@
 export PLUG_DIR=$HOME/.zim
 if [[ ! -d $PLUG_DIR ]]; then
-	curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-	rm ~/.zimrc
-	ln -s ~/.config/zsh/zimrc ~/.zimrc
+    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+    rm ~/.zimrc
+    ln -s ~/.config/zsh/zimrc ~/.zimrc
+    echo "source ~/.config/zsh/zshrc">>~/.zshrc
 fi
