c6b5a164dc3df6788c9c951e70c5fcc4157e0250
zachary <zlj19971222@outlook.com>
2021-07-07 15:25:45 +0800

    feat: add new alias fro `npm`.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,3 +23,24 @@ alias s="neofetch --cpu_temp C --memory_display barinfo --shell_path on "
 #alias s='neofetch --cpu_temp C --memory_display barinfo --shell_path on --ascii "$(fortune | cowsay -W 35 | lolcat)"'
 
 eval $(thefuck --alias)
+
+# npm
+alias ni='npm install'
+alias nis='npm install --save'
+alias nid='npm install --save-dev'
+alias nit='npm install-test'
+alias nits='npm install-test --save'
+alias nitd='npm install-test --save-dev'
+alias nu='npm uninstall'
+alias nus='npm uninstall --save'
+alias nusd='npm uninstall --save-dev'
+alias np='npm publish'
+alias nup='npm unpublish'
+alias nlk='npm link'
+alias nod='npm outdated'
+alias nrb='npm rebuild'
+alias nud='npm update'
+alias nr='npm run'
+alias nls='npm list --depth=0 2>/dev/null'
+alias nlsg='npm list -g --depth=0 2>/dev/null'
+alias nt='npm test'
