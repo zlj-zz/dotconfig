5c25b7e2a14921c2c8dc35400f594ce6d6891d7a
zachary <zlj19971222@outlook.com>
2021-09-26 15:38:09 +0800

    refactor.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,10 @@
+#!/usr/bin/env zsh
+
 export PLUG_DIR=$HOME/.zim
 if [[ ! -d $PLUG_DIR ]]; then
     curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
     rm ~/.zimrc
-    ln -s ~/.config/zsh/zimrc ~/.zimrc
-    echo "source ~/.config/zsh/zshrc">>~/.zshrc
+    ln -s ~/.configrc/zsh/zimrc ~/.zimrc
+    echo "source ~/.configrc/zsh/zshrc">>~/.zshrc
 fi
+
