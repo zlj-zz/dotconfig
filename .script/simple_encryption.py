bbe18681b8017818f7b6b56a69844b6a45152cab
Lijun Zhang <lijunzhang@Lijuns-MacBook-Pro.local>
2022-01-12 14:44:51 +0800

    update simple_encryption

--- .script/simple_encryption.py
+++ .script/simple_encryption.py
@@ -17,5 +17,20 @@ def decode(passed):
     return new_code
 
 
+def main():
+    while True:
+        choice = input("1-encrpyt, 2-decode; input:").strip()
+        if choice in ['1', '2']:
+            break
+        print('error input, try again.')
+
+    code = input("input your code:")
+
+    if choice == '1':
+        print(encrpyt(code))
+
+    elif choice == '2':
+        print(decode(code))
+
 if __name__ == "__main__":
-    pass
+    main()
